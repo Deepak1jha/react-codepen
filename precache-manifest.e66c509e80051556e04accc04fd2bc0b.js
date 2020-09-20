55722cfd956cb24f2d8f594a9139dae433fc81a0
Deepak1jha <ddjha171@gmail.com>
2020-09-20 16:49:12 +0530

    Updates

---
+++ precache-manifest.e66c509e80051556e04accc04fd2bc0b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad9ab4d11bfb591930f8b3a1311654b5",
+    "url": "/react-codepen/index.html"
+  },
+  {
+    "revision": "75063999ae8b47e351a8",
+    "url": "/react-codepen/static/css/2.e980ac4b.chunk.css"
+  },
+  {
+    "revision": "2b7a3c2ea52f21dd69f8",
+    "url": "/react-codepen/static/css/main.0a3653d8.chunk.css"
+  },
+  {
+    "revision": "75063999ae8b47e351a8",
+    "url": "/react-codepen/static/js/2.9027c9a9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-codepen/static/js/2.9027c9a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b7a3c2ea52f21dd69f8",
+    "url": "/react-codepen/static/js/main.f03264cf.chunk.js"
+  },
+  {
+    "revision": "1819c92a20dc8038edf3",
+    "url": "/react-codepen/static/js/runtime-main.97764405.js"
+  }
+]);
